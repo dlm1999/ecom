8755ded6d1bdadb5a41c90891cbf1cfe677ef8fb
dlm1999 <65058261+dlm1999@users.noreply.github.com>
2022-03-08 23:42:20 +0530

    Add files via upload

---
+++ src/component/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+const Cart = ()=>{
+	return(
+		<>
+			<div className = "container my-5 py-5">
+				<div className="row">
+						
+				</div>
+			</div>
+		</>		
+	);
+}
+
+export default Cart;
